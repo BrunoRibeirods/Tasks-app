e597863d8d191dff494d60a3ed8e582f9c53eeec
Bruno Ribeiro <brunorbds50@gmail.com>
2021-02-20 17:32:19 -0300

    Changing layout to add type of task

--- app/src/main/java/dev/brunoribeiro/ajuste/ui/TaskAddFragment.kt
+++ app/src/main/java/dev/brunoribeiro/ajuste/ui/TaskAddFragment.kt
@@ -1,5 +1,6 @@
 package dev.brunoribeiro.ajuste.ui
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import dev.brunoribeiro.ajuste.R
 import dev.brunoribeiro.ajuste.databinding.FragmentHomeBinding
 import dev.brunoribeiro.ajuste.databinding.FragmentTaskAddBinding
 import dev.brunoribeiro.ajuste.entities.Task
@@ -19,6 +21,7 @@ class TaskAddFragment : Fragment() {
     private var _binding: FragmentTaskAddBinding? = null
     private val binding get() = _binding!!
     lateinit var repository: ServiceRepository
+
 
     val viewModel by viewModels<TaskAddViewModel>(){
         object: ViewModelProvider.Factory{
@@ -36,21 +39,39 @@ class TaskAddFragment : Fragment() {
         _binding = FragmentTaskAddBinding.inflate(inflater, container, false)
         repository = ServiceRepository.getInstance(requireContext())
         binding.toolbarAddTask.setNavigationOnClickListener { activity?.onBackPressed() }
+        var type = 0
+
+        binding.typeUrgente.setOnClickListener {
+            type = Task.URGENTE
+            binding.typeName.text = " Urgente"
+            binding.typeName.setTextColor(binding.root.resources.getColor(R.color.red))
+        }
+        binding.typeIntermediario.setOnClickListener {
+            type = Task.INTERMEDIARIO
+            binding.typeName.text = " Intermediario"
+            binding.typeName.setTextColor(binding.root.resources.getColor(R.color.yellow))
+        }
+        binding.typeNormal.setOnClickListener {
+            type = Task.NORMAL
+            binding.typeName.text = " Normal"
+            binding.typeName.setTextColor(binding.root.resources.getColor(R.color.blue))
+        }
+
 
         binding.btnAddTask.setOnClickListener {
-            if (binding.etTaskAdd.text.isNullOrEmpty()){
+            if (binding.etTaskAdd.text.isNullOrEmpty() && type == 0){
                 Toast.makeText(context, "Campos vazios", Toast.LENGTH_SHORT).show()
             }else{
-                sendTask(getTask(binding.etTaskAdd.text.toString()))
+                sendTask(getTask(binding.etTaskAdd.text.toString(), type))
             }
 
         }
         return binding.root
     }
 
-    fun getTask(title: String): Task{
+    fun getTask(title: String, type: Int): Task{
 
-        return Task(null, title)
+        return Task(null, title, type)
     }
 
     fun sendTask(task: Task){
