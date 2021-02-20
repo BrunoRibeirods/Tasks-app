fa15a6d8090beb3106d988d609b3a4e8c3b8f9b9
Bruno Ribeiro <brunorbds50@gmail.com>
2021-02-20 17:31:55 -0300

    Adding type to task

--- app/src/main/java/dev/brunoribeiro/ajuste/entities/Task.kt
+++ app/src/main/java/dev/brunoribeiro/ajuste/entities/Task.kt
@@ -8,5 +8,12 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "actual_tasks")
 data class Task(
     @PrimaryKey(autoGenerate = true)val room_id: Int? = null,
-    @ColumnInfo(name = "title")val title: String?
-)
+    @ColumnInfo(name = "title")val title: String?,
+    @ColumnInfo(name = "type")val type: Int?
+){
+    companion object{
+        val URGENTE = 1
+        val INTERMEDIARIO = 2
+        val NORMAL = 3
+    }
+}
