060b5dc6856157c31092e5814ca3bbf5a2b68a7e
Bruno Ribeiro <brunorbds50@gmail.com>
2021-02-22 16:55:53 -0300

    Adding removeAt to delete tasks

--- app/src/main/java/dev/brunoribeiro/ajuste/adapters/TasksAdapter.kt
+++ app/src/main/java/dev/brunoribeiro/ajuste/adapters/TasksAdapter.kt
@@ -3,12 +3,15 @@ package dev.brunoribeiro.ajuste.adapters
 import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView
 import dev.brunoribeiro.ajuste.R
 import dev.brunoribeiro.ajuste.entities.Task
 import dev.brunoribeiro.ajuste.databinding.ItemTaskBinding
+import dev.brunoribeiro.ajuste.repository.ServiceRepository
+import dev.brunoribeiro.ajuste.ui.HomeViewModel
 
-class TasksAdapter(private val listOfTasks: List<Task>): RecyclerView.Adapter<TasksAdapter.TaskViewHolder>() {
+class TasksAdapter(private val listOfTasks: MutableList<Task>, val deleteDao: HomeViewModel): RecyclerView.Adapter<TasksAdapter.TaskViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         return TaskViewHolder.from(parent)
     }
@@ -16,6 +19,13 @@ class TasksAdapter(private val listOfTasks: List<Task>): RecyclerView.Adapter<Ta
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) = holder.bind(listOfTasks[position])
 
     override fun getItemCount(): Int = listOfTasks.size
+
+    fun removeAt(position: Int) {
+        deleteDao.deleteTask(listOfTasks[position])
+        listOfTasks.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
 
     class TaskViewHolder(val binding: ItemTaskBinding): RecyclerView.ViewHolder(binding.root){
 
@@ -46,6 +56,8 @@ class TasksAdapter(private val listOfTasks: List<Task>): RecyclerView.Adapter<Ta
             return ColorStateList.valueOf(binding.root.resources.getColor(result))
         }
     }
+
+
 
 
 
