de9815fc13bb628f98c0716857234b6f4d2a1358
Bruno Ribeiro <brunorbds50@gmail.com>
2021-02-23 14:10:00 -0300

    Updating Database Room

--- app/src/main/java/dev/brunoribeiro/ajuste/database/AppDataBase.kt
+++ app/src/main/java/dev/brunoribeiro/ajuste/database/AppDataBase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import dev.brunoribeiro.ajuste.entities.Task
 
-@Database(entities = [Task::class], version = 2, exportSchema = false)
+@Database(entities = [Task::class], version = 4, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
     abstract val TaskDatabaseDao: TaskDao
 
