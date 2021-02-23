4cd98629775aa1f4019695bd20fb8cc264aeff55
Bruno Ribeiro <brunorbds50@gmail.com>
2021-02-23 14:12:15 -0300

    Add check to Task Class

--- app/src/main/java/dev/brunoribeiro/ajuste/entities/Task.kt
+++ app/src/main/java/dev/brunoribeiro/ajuste/entities/Task.kt
@@ -7,9 +7,10 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "actual_tasks")
 data class Task(
-    @PrimaryKey(autoGenerate = true)val room_id: Int? = null,
+    @PrimaryKey(autoGenerate = true)@ColumnInfo(name = "id")val room_id: Int? = null,
     @ColumnInfo(name = "title")val title: String?,
-    @ColumnInfo(name = "type")val type: Int?
+    @ColumnInfo(name = "type")val type: Int?,
+    @ColumnInfo(name = "check_value")var checked: Boolean? = false
 ){
     companion object{
         val URGENTE = 1
